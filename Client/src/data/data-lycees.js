8993c34ce5711ca1b06350dfd36548ee08c64560
GilianC <163392501+GilianC@users.noreply.github.com>
2024-12-17 14:34:42 +0100

    Update-Final

--- Client/src/data/data-lycees.js
+++ Client/src/data/data-lycees.js
@@ -2,34 +2,81 @@
 
 let data = await fetch("./src/data/json/lycees.json");
 data = await data.json();
+import {Candidats} from "./data-candidats.js";
 
+
+let compare = function(a, b){
+    if (a.numero_uai < b.numero_uai){
+        return -1;
+    }
+    if (a.numero_uai > b.numero_uai){
+        return 1;
+    }
+    return 0;
+}
+data.sort(compare);
 let Lycees = {}
-
 Lycees.getAll = function(){ 
     return data;
 }
+
+Lycees.binarySearch = function(numero_uai){
+    let left = 0;
+    let right = data.length - 1;
+    let mid;
+    while (left <= right){
+         mid = Math.floor((left + right) / 2);
+        if (data[mid].numero_uai == numero_uai){
+            return mid;
+        }
+        if (data[mid].numero_uai < numero_uai){
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return null;
+}
+
+console.log(data);
 // Lycees.getLycee= function(){
 //     let res = [];
 //     for (let obj of data) {
+//         let num = obj.numero_uai;
 //         let nom = obj.appellation_officielle;
 //         // console.log(nom);
 //             let lat = parseFloat(obj.latitude);
 //             let long = parseFloat(obj.longitude);
-//             res.push([lat, long, nom]);
+//             res.push([lat, long, num, nom]);
 //     }
 // console.log(res);
 // return res;
-Lycees.getLycee= function(){
-    let res = [];
-    for (let obj of data) {
+
+// }
+//fais la fonction getLycee qui retourne les lycées avec leur latitude et longitude selon leur présence dans la liste des candidats en utilisant getDiplomeEnPreparation de data-candidats.js
+
+Lycees.getLycee = function() {
+let res = [];
+let candidats = Candidats.getDiplomeEnPreparation();
+candidats.forEach(candidat => {
+    let index = Lycees.binarySearch(candidat.Scolarite[0].UAIEtablissementorigine);
+    if (index !== null) {
+        let obj = data[index];
         let num = obj.numero_uai;
-        // console.log(nom);
-            let lat = parseFloat(obj.latitude);
-            let long = parseFloat(obj.longitude);
-            res.push([lat, long, num]);
+        let nom = obj.appellation_officielle;
+        let lat = parseFloat(obj.latitude);
+        let long = parseFloat(obj.longitude);
+        if (lat != null && long != null && !isNaN(lat) && !isNaN(long)) {
+            let alreadyExists = res.some(item => item[2] === num);
+            if (!alreadyExists) {
+                res.push([lat, long, num, nom]);
+            }
+        }
     }
+});
 console.log(res);
-return res;
+    return res;
 
 }
 export { Lycees };
--- Client/src/ui/ite-5/index.js
+++ Client/src/ui/ite-5/index.js
@@ -2,7 +2,7 @@
 
 let Ite5 = {};
 
-Ite5.render = function(lycee, candidate){
+Ite5.render = function(lycee){
 
     var map = L.map('map').setView([45.835783764063905, 1.2311845477920846], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -14,13 +14,13 @@ Ite5.render = function(lycee, candidate){
     // on vérifie si les lycée de la liste lycee sont présent dans candidate
     let res = [];
     for (let obj of lycee) {
-        for (let i = 0; i < candidate.length; i++) {
-            if (obj[2] == candidate[i] && obj[0] != null && obj[1] != null && !isNaN(obj[0]) && !isNaN(obj[1])) { 
+            if ( obj[0] != null && obj[1] != null && !isNaN(obj[0]) && !isNaN(obj[1])) { 
                 // console.log(obj[2]);
-                // console.log(candidate[i]);
+
                 res.push(obj[2]);
-                L.marker([obj[0], obj[1]]).addTo(map).bindPopup('Lycée présent dans les deux listes');
-            }
+                L.marker([obj[0], obj[1]]).addTo(map).bindPopup(obj[3] + " avec "   );
+
+            
         }
     }
     console.log(res);
