238149da846e9c8e553c88343a726cea8837100b
GilianC <163392501+GilianC@users.noreply.github.com>
2024-12-20 08:11:27 +0100

    Update index.js

--- Client/src/ui/Graph/index.js
+++ Client/src/ui/Graph/index.js
@@ -13,33 +13,33 @@ let GraphView = {
     
     renderGraph : function(data){
 
-        const threshold = parseInt(document.querySelector("#slider").value);
+        // const threshold = parseInt(document.querySelector("#slider").value);
 
-        // Regrouper les départements avec moins de candidatures que le seuil
-        let groupedData = [];
-        let otherDepartments = { STI2D: 0, Générale: 0, autres: 0, total: 0 };  // Pour regrouper les départements "autres"
+        // // Regrouper les départements avec moins de candidatures que le seuil
+        // let groupedData = [];
+        // let otherDepartments = { STI2D: 0, Générale: 0, autres: 0, total: 0 };  // Pour regrouper les départements "autres"
 
-        data.forEach(item => {
-            const totalCandidates = item[4].STI2D + item[4].Générale + item[4].autres;
-            if (totalCandidates <= threshold) {
-                // Si le total des candidats est inférieur ou égal au seuil, on les regroupe
-                otherDepartments.STI2D += item[4].STI2D;
-                otherDepartments.Générale += item[4].Générale;
-                otherDepartments.autres += item[4].autres;
-                otherDepartments.total += totalCandidates;
-            } else {
-                // Sinon, on garde le département
-                groupedData.push(item);
-            }
-        });
+        // data.forEach(item => {
+        //     const totalCandidates = item[4].STI2D + item[4].Générale + item[4].autres;
+        //     if (totalCandidates <= threshold) {
+        //         // Si le total des candidats est inférieur ou égal au seuil, on les regroupe
+        //         otherDepartments.STI2D += item[4].STI2D;
+        //         otherDepartments.Générale += item[4].Générale;
+        //         otherDepartments.autres += item[4].autres;
+        //         otherDepartments.total += totalCandidates;
+        //     } else {
+        //         // Sinon, on garde le département
+        //         groupedData.push(item);
+        //     }
+        // });
 
-        // Si des départements sont regroupés, on ajoute un élément "Autres départements"
-        if (otherDepartments.total > 0) {
-            groupedData.push([
-                null, null, null, 'Autres départements', // Latitude, Longitude et code postal non utilisés ici
-                otherDepartments, otherDepartments.total
-            ]);
-        }
+        // // Si des départements sont regroupés, on ajoute un élément "Autres départements"
+        // if (otherDepartments.total > 0) {
+        //     groupedData.push([
+        //         null, null, null, 'Autres départements', // Latitude, Longitude et code postal non utilisés ici
+        //         otherDepartments, otherDepartments.total
+        //     ]);
+        // }
 
         data.sort((a, b) => {
             let totalA = a[4].STI2D + a[4].Générale + a[4].autres;
